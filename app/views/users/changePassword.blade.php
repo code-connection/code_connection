9e8f25c8c144a53fb9964acee2cec44d6d7fc524
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-04-29 10:33:00 -0500

    changed update password rules to password|confirmed. created update password page

--- app/views/users/changePassword.blade.php
+++ app/views/users/changePassword.blade.php
@@ -1,55 +1,159 @@
 
 @extends('layouts.master')
 
+
+   <title>Accounts Page</title>
+
     @section('top-script')
 
-        <title>Change Password</title>
+    <style>
 
+    .container {
+      margin-top: 20px;
+      margin-bottom: 50px;
+    }
+
+      .panel-default .panel-heading {
+      background-color: #6C8096;
+      border-color: #506378;
+      color: #F1F1F1;
+      }
+
+      h3 {
+      margin-top: 6px;
+      }
+
+      .account-box {
+        background-color: #F1F1F1;
+        color: #777777;
+        padding: 15px;
+
+        margin-bottom: 20px;
+      }
+
+      .account-info {
+        padding-top: 15px;
+        padding-bottom: 30px;
+      }
+
+      .btn-default {
+      background-color: #F67366;
+      color:white;
+      }
+
+      .btn-default:hover {
+      background-color: #F67366;
+      color:white;
+      -webkit-transition: all 500ms ease;
+      -moz-transition: all 500ms ease;
+      -ms-transition: all 500ms ease;
+      -o-transition: all 500ms ease;
+      transition: all 500ms ease;
+      }
+
+      .account-txt {
+        padding-bottom: 30px;
+      }
+
+      .panel-body {
+        border: 1.5px solid;
+        border-color: #506378;
+      }
+
+      .state {
+        right:1em;
+      }
+
+      
+    .chosen-container-single .chosen-single {
+            position: relative;
+            right: 1em;
+    }
+
+    .fa-lock {
+      color:#777777;
+    }
+    
+
+   
+     
+
+    </style>
+      
+   
     @stop
 
     @section('content')
 
-	<div class="account-form container">	
-		<div class="col-md-4 account-txt">
-    		<h1 id="account-direction">Account</h1>
-    		
-    		<h4 class="account-info">Made a mistake entering your information?
-    			No Worries! Follow the instructions below:</h4>
-    	    <div class='line'>
-          	</div>
-          	<h4 class="account-info"> Step 1. On your right, you will see your current information. To 
-     			make a change, just type in the fields you wish to correct. Yes, it's as easy as that!
-     		</h4>
-     	    <div class='line'>
-            </div>	
-     		<h4 class="account-info"> Step 2. Review Your changes. Are they correct?</h4>
-    		<div class='line'>
-            </div>
-     		<h4 class="account-info"> Step 3. If so, Congrats! Just click on the submit button, You have
-     			now officially edited your information.</h4>
- 		</div> <!-- end col-md-4 -->
+<div class="container">
 
- 	    <div class='line'></div>	
+  <div class="panel panel-default">
+      <div class="panel-heading"><h3>Account</h3></div>
+        <div class="panel-body">
 
-        <div class="col-md-8">
+        <div class="col-md-12 account-txt">
+          
+          <div class = "form-group">
 
-            {{ Form::open(array('action' => array('UserController@doChangePassword', $user->id), 'method' => 'PUT', 'class' => 'form-horizontal')) }} 
+            <h4 class="account-info"> 
+            Made a mistake putting in your information?
+            No Worries! We've got you covered.
+            <br>
+            <br>
+                Step 1. On your right, you will see your original information. To 
+            make a change, just type in the fields you wish to correct.
+            <br>
+            <br>
+            Step 2. Review Your changes. Are they correct?
+            <br>
+            <br>
+            Step 3. Just click on the submit button. You have
+            now officially edited your information.
+            </h4>
 
-                <h3 class="sign-placeholders">Password</h3>
+           
 
-                <textarea type="password" class="form-control form1" name="password" aria-describedby="basic-addon1"></textarea>
+          
+          </div>
+          
+        <div class="account-box"><h4><i class="fa fa-lock" aria-hidden="true"></i> Update Password</h4></div>
+        <div class="col-md-2"></div>
+          <div class="col-md-8">
 
+            {{ Form::open(array('action' => array('UserController@doChangePassword', $user->id), 'method' => 'PUT', 'class' => 'form-horizontal')) }}
+                 
+               {{ Form::label('password', 'Password') }}
+                {{ Form::password('password', ['class'=> 'form-control', 'placeholder'=>"Enter password"]) }}
+                @if ($errors->has('password'))
+                <p>{{{$errors->first('password')}}}</p>
+                @endif
+            
 
-                <h3 class="sign-placeholders">Confirm Password</h3>
-                <span> 
+                 
+            {{ Form::label('password', 'Confirm Password') }}
+              {{ Form::password('password_confirmation', ['class'=> 'form-control', 'placeholder'=>"Confirm password"]) }}
+              @if ($errors->has('password'))
+              <p>{{{$errors->first('password')}}}</p>
+              @endif
+              
 
-                <textarea type="password" class="form-control form1"  name="confirmPassword" aria-describedby="basic-addon1"></textarea>
-                <button  id="edit-btn" name="edit-btn" type="submit" class="btn btn-default">Submit</button>
+            
+            
+           
+            <br>
+            <br>
+            <button type="submit" class="btn btn-default" name="save" value="save">update</button>
             {{Form::close()}}
-   
-		</div> <!-- end col-md-8 -->
-    @stop
 
-    @section('bottom-script')
- 	
-    @stop
+          </div>
+      </div> 
+    </div>
+
+  </div>  <!-- end panel -->
+</div>
+  
+  @stop
+
+  @section('bottom-script')
+ 
+    
