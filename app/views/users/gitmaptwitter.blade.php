0599a180492f17f7077d52170f96929f845e0bc2
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-05-02 09:32:21 -0500

    attempting to incorporate stroll.js into gitmaptwitter

--- app/views/users/gitmaptwitter.blade.php
+++ app/views/users/gitmaptwitter.blade.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
   <!-- order matters, my own stylesheet must go unederneath -->
+    <link rel="stylesheet" href="css/stroll.css">
   
   <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 
@@ -203,7 +204,7 @@ header{
      <!--html here-->
 
     </div> <!-- end wrapper   --> 
-
+<script src="js/stroll.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
     </script>
