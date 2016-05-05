1c2e686bbfd3e776d2b9de96e8463d048af4c9b8
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-05-05 12:55:41 -0500

    added image role to user table

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -5,22 +5,25 @@ class UserTableSeeder extends Seeder{
 
 	public function run(){
 
+
 		$user = new User();
-		$user->first_name = $_ENV['USER_FIRST'];
-		$user->last_name = $_ENV['USER_LAST'];
-		$user->email = $_ENV['USER_EMAIL'];
-		$user->password = $_ENV['USER_PASS'];
+		$user->first_name = 'Don';
+		$user->last_name = 'Moore';
+		$user->email = 'dmoore@codeup.com';
+		$user->password = 'yanula20';
 		$user->city = 'San Antonio';
 		$user->state = 'TX';
 		$user->image = '/img/don.jpeg';
-
 		$user->save();
 
+
+  
+
 		$user2 = new User();
-		$user2->first_name = 'Richard';
-		$user2->last_name = 'De Los Santos';
-		$user2->email = 'RichardDeLosSantos1292@gmail.com';
-		$user2->password = 'DelosSantos0712';
+		$user2->first_name = $_ENV['USER_FIRST'];
+		$user2->last_name = $_ENV['USER_LAST'];
+		$user2->email = $_ENV['USER_EMAIL'];
+		$user2->password = $_ENV['USER_PASS'];
 		$user2->city = 'San Antonio';
 		$user2->state = 'TX';
 		$user2->image = '/img/ricardo1.jpeg';
