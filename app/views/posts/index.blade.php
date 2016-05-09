06d13742e43b422154f10ebe5e0e225134e863d8
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-05-09 11:26:25 -0500

    added everyone's links to twitter, linkedin... etc

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -63,10 +63,10 @@
 		        <h3>Donald Moore</h3>
 		        <p>A real American Hero</p>
 		        <div id='footer-icons1' class="icon-txt">
-				<a href=""><i  class="fa fa-twitter fa-2x twitter-icon" target="_blank"></i></a>
+				<a href="https://twitter.com/seetheworld1989" target="_blank"><i  class="fa fa-twitter fa-2x twitter-icon" ></i></a>
 				<a href=""><i class="fa fa-facebook-official fa-2x facebook-icon"></i></a>
-				<a href=""><i class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
-				<a href=""><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
+				<a href="https://www.linkedin.com/in/donald-moore-b575a235?trk=nav_responsive_tab_profile" target="_blank"><i class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
+				<a href="mailto:donmoore26762671@gmail.com"><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
 				</div>
 		      </div>
 		    </div>
@@ -78,10 +78,10 @@
 		        <h3>Richard De Los Santos</h3>
 		        <p>Looking for the perfect burger</p>
 		        <div id='footer-icons1' class="icon-txt">
-				<a href=""><i  class="fa fa-twitter fa-2x twitter-icon" target="_blank"></i></a>
+				<a href="https://twitter.com/lowliferichard" target="_blank"><i  class="fa fa-twitter fa-2x twitter-icon"></i></a>
 				<a href=""><i class="fa fa-facebook-official fa-2x facebook-icon"></i></a>
-				<a href=""><i class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
-				<a href=""><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
+				<a href="https://www.linkedin.com/in/ricardo-de-los-santos-42b73a107?trk=nav_responsive_tab_profile" target="_blank"><i class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
+				<a href="mailto:RichardDeLosSantos1292@gmail.com"><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
 				</div>
 		      </div>
 		    </div>
@@ -93,10 +93,10 @@
 		        <h3>Trey Sowers</h3>
 		        <p>A man with the plan</p>
 		        <div id='footer-icons1' class="icon-txt">
-				<a href=""><i  class="fa fa-twitter fa-2x twitter-icon" target="_blank"></i></a>
-				<a href=""><i class="fa fa-facebook-official fa-2x facebook-icon"></i></a>
-				<a href=""><i class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
-				<a href=""><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
+				<a href="https://twitter.com/ItsTreySucka" target='_blank'><i  class="fa fa-twitter fa-2x twitter-icon"></i></a>
+				<a href="https://www.facebook.com/sowers.trey" target="_blank" ><i class="fa fa-facebook-official fa-2x facebook-icon"></i></a>
+				<a href="https://www.linkedin.com/in/george-sowers-763640104?trk=nav_responsive_tab_profile_pic" target="_blank" ><i  class="fa fa-linkedin-square fa-2x linked-icon"></i></a>
+				<a href="mailto:georgevsowers@gmail.com"><i class="fa fa fa-envelope-o fa-2x mail-icon"></i></a>
 				</div>
 		      </div>
 		    </div>
