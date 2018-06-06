ea758e3502e1af8553c9686a0637bb52f7657679
Donald Moore <donmoore26762671@gmail.com>
2018-06-06 15:30:43 -0500

    trying to navigate instagram media json

--- app/views/users/gitmaptwitter.blade.php
+++ app/views/users/gitmaptwitter.blade.php
@@ -977,10 +977,10 @@ function getGitHubData(language,address){
                 d.data.forEach(function (element, index, array) {
                     console.log(element.id);
                 if(element.id != 0) {
-                    $.get("https://api.instagram.com/v1/locations/"+element.id+"/media/recent?access_token=3212180381.ab944e6.af0e8cf707884696a1d86592f64c47e8").done(function(media) {
+                    $.get("https://api.instagram.com/v1/locations/"+element.id+"/media/recent?access_token=3212180381.ab944e6.af0e8cf707884696a1d86592f64c47e8").done(function(data) {
 
                        
-                        console.log(media.data);
+                        console.log(data);
 
                          // media.data.forEach(function (element, index, array) { 
                            
