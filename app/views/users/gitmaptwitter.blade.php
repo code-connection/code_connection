0c0642f9f7da12aa87496a0c0a46e4455d8674f7
Donald Moore <donmoore26762671@gmail.com>
2018-06-05 23:44:47 -0500

    working on 2nd get request

--- app/views/users/gitmaptwitter.blade.php
+++ app/views/users/gitmaptwitter.blade.php
@@ -979,8 +979,8 @@ function getGitHubData(language,address){
                 d.data.forEach(function (element, index, array) {
                     //console.log(element.id);
                 if(element.id != 0) {
-                    $.get("https://api.instagram.com/v1/locations/"+element.id+"/media/recent?access_token=3212180381.ab944e6.af0e8cf707884696a1d86592f64c47e8").done(function(media) {
-                        console.log(media.data.likes.count);
+                    $.get("https://api.instagram.com/v1/locations/"+element.id+"/media/recent?access_token=3212180381.ab944e6.af0e8cf707884696a1d86592f64c47e8").done(function(data) {
+                        console.log(data.likes.count);
                     }).fail(function() {
                         console.log("Instagram Media error!");
                     }).always(function() {
