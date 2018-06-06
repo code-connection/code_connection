3930d4e7a00fa9421b15736491cc8804a48d245e
Donald Moore <donmoore26762671@gmail.com>
2018-06-06 15:20:05 -0500

    trying to navigate instagram media json

--- app/views/users/gitmaptwitter.blade.php
+++ app/views/users/gitmaptwitter.blade.php
@@ -975,19 +975,22 @@ function getGitHubData(language,address){
 
                 var instagramMediaResults = '<ul class="wave">';
                 d.data.forEach(function (element, index, array) {
-                    //console.log(element.id);
+                    console.log(element.id);
                 if(element.id != 0) {
                     $.get("https://api.instagram.com/v1/locations/"+element.id+"/media/recent?access_token=3212180381.ab944e6.af0e8cf707884696a1d86592f64c47e8").done(function(media) {
 
-                         media.data.forEach(function (element, index, array) { 
+                        var len = media.data.length;
+                        console.log(len);
+
+                         // media.data.forEach(function (element, index, array) { 
                            
-                            instagramMediaResults += "<li class=\"data-lists\">"+"<img src=\"/img/instagram.png\">"
-                            +"Description: "+index[type]+"<br>"
-                            +"Description: "+element[type]+"<br>"
-                            +"Description: "+array.type+"<br>"
-                            +"Description: "+index[caption].text+"<br>"
-                            +"</li>"+"<br>"
-                         }); //end foreach
+                         //    instagramMediaResults += "<li class=\"data-lists\">"+"<img src=\"/img/instagram.png\">"
+                         //    +"Description: "+element.type+"<br>"
+                         //    +"Description: "+element[type]+"<br>"
+                         //    +"Description: "+array.type+"<br>"
+                         //    +"Description: "+index[caption].text+"<br>"
+                         //    +"</li>"+"<br>"
+                         // }); //end foreach
 
 
                       $("#insert_instagram_media").html(instagramMediaResults);
