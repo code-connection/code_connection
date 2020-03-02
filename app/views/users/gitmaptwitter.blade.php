3a1ab4b3a2c8e6a9fc5a99e607d317f6333d688e
Donald Moore <donmoore26762671@gmail.com>
2020-03-02 11:14:05 -0600

    added a new api key

--- app/views/users/gitmaptwitter.blade.php
+++ app/views/users/gitmaptwitter.blade.php
@@ -258,8 +258,7 @@
         <div class="panel-heading"><h2>Code Connection</h2></div>
             <div class="panel-body">
                 <div class="row">
-
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJMMD6k9NVgkv5xDAcIaENzanI8psOI44"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx9bs-sB89p9D91xh-pJvB3-Wby7iNs_k"></script>
            
                 <br>
                     <div class="col-md-3" id="coordinates">
